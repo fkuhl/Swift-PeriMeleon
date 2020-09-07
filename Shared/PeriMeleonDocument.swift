32139e3d9e8c08965247016509f0fc3837d81ad9
Frederick Kuhl <fkuhl@tyndalesoft.com>
2020-09-07 15:12:05 -0400

    ready to add UI

--- Shared/PeriMeleonDocument.swift
+++ Shared/PeriMeleonDocument.swift
@@ -15,19 +15,23 @@ extension UTType {
 
 struct PeriMeleonDocument: FileDocument {
     var households = [Household]()
+    var activeHouseholds = [Household]()
+    var members = [Member]()
+    var activeMembers = [Member]()
 
     static var readableContentTypes: [UTType] { [.periMeleonRollsDocument] }
-    
+    static var writableContentTypes: [UTType] { [.periMeleonRollsDocument] }
+
     init() {
         self.households = [Household]()
     }
     
-    init(fileWrapper: FileWrapper, contentType: UTType) throws {
-        if let data = fileWrapper.regularFileContents {
-            self.households = try jsonDecoder.decode([Household].self, from: data)
-            NSLog("read \(self.households.count) households")
-        }
-    }
+//    init(fileWrapper: FileWrapper, contentType: UTType) throws {
+//        if let data = fileWrapper.regularFileContents {
+//            self.households = try jsonDecoder.decode([Household].self, from: data)
+//            NSLog("read \(self.households.count) households from init fileWrapper")
+//        }
+//    }
 
 
     init(configuration: ReadConfiguration) throws {
@@ -37,24 +41,47 @@ struct PeriMeleonDocument: FileDocument {
             return
         }
         do {
-            self.households = try jsonDecoder.decode([Household].self, from: data)
-            NSLog("read \(self.households.count) households")
+            let unsortedHouseholds = try jsonDecoder.decode([Household].self, from: data)
+            households = unsortedHouseholds.sorted {
+                $0.head.fullName() < $1.head.fullName()
+            }
+            NSLog("read \(self.households.count) households from init config")
+            activeHouseholds = households.filter { $0.head.status.isActive() }
+            NSLog("found \(activeHouseholds.count) active households")
+            let unsortedMembers = pullMembers(from: households)
+            members = unsortedMembers.sorted{
+                $0.fullName() < $1.fullName()
+            }
+            NSLog("found \(members.count) members")
+            activeMembers = members.filter{ $0.status.isActive()}
+            NSLog("found \(activeMembers.count) active mambers")
         } catch {
             let err = error as! DecodingError
             NSLog("decode error \(err)")
         }
     }
     
-    //which of these?
-    func write(to fileWrapper: inout FileWrapper, contentType: UTType) throws {
-        let data = try jsonEncoder.encode(self.households)
-        NSLog("writing \(data.count) bytes")
-        fileWrapper = FileWrapper(regularFileWithContents: data)
-    }
+//    func write(to fileWrapper: inout FileWrapper, contentType: UTType) throws {
+//        let data = try jsonEncoder.encode(self.households)
+//        NSLog("writing \(data.count) bytes")
+//        fileWrapper = FileWrapper(regularFileWithContents: data)
+//    }
     
     func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
         let data = try jsonEncoder.encode(self.households)
         NSLog("writing \(data.count) bytes")
         return .init(regularFileWithContents: data)
+    }
+    
+    private func pullMembers(from households: [Household]) -> [Member] {
+        var members = [Member]()
+        households.forEach { household in
+            members.append(household.head)
+            if let spouse = household.spouse {
+                members.append(spouse)
+            }
+            members.append(contentsOf: household.others)
+        }
+        return members
     }
 }
