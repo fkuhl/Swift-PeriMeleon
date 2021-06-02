27499afae71e1177914dad8a823c30583c7cb8dd
Frederick Kuhl <fkuhl@tyndalesoft.com>
2021-06-02 09:32:50 -0400

    UI tweak

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -29,9 +29,11 @@ struct ContentView: View {
                          document: $document,
                          buttonText: "Open")
         case .cannotDecode(let d1, let d2, let d3):
-            cannotDecode(basicError: d1, codingPath: d2, underlyingError: d3)
+            swanSong(lastGasp: "Alas, the file cannot be decoded.",
+                         basicError: d1, codingPath: d2, underlyingError: d3)
         case .saveError(let d1, let d2, let d3):
-            saveError(basicError: d1, codingPath: d2, underlyingError: d3)
+            swanSong(lastGasp: "Alas, the file could not be saved.",
+                     basicError: d1, codingPath: d2, underlyingError: d3)
         case .passwordEntriesDoNotMatch:
             PasswordView(label: "Passwords didn't match. Please enter again:",
                          document: $document.animation(),
@@ -39,32 +41,23 @@ struct ContentView: View {
         }
     }
     
-    private func cannotDecode(basicError: String,
-                              codingPath: String,
-                              underlyingError: String) -> some View {
+    private func swanSong(lastGasp: String,
+                          basicError: String,
+                          codingPath: String,
+                          underlyingError: String) -> some View {
         VStack(alignment: .leading) {
-            Text("Alas, the file cannot be decoded.").font(.headline)
+            Text(lastGasp).font(.headline)
             Text("This probably should be brought to the attention of the developer.")
                 .font(.body)
             Text("Some additional information:").font(.body)
             Text("Basic error: " + basicError).font(.body)
-            Text("Coding path, if any: " + codingPath).font(.body)
-            Text("Underlying error: " + underlyingError).font(.body)
-        }
-        .padding()
-    }
-    
-    private func saveError(basicError: String,
-                           codingPath: String,
-                           underlyingError: String) -> some View {
-        VStack(alignment: .leading) {
-            Text("Alas, the file could not be saved.").font(.headline)
-            Text("This probably should be brought to the attention of the developer.")
-                .font(.body)
-            Text("Some additional information:").font(.body)
-            Text("Basic error: " + basicError).font(.body)
-            Text("Coding path, if any: " + codingPath).font(.body)
-            Text("Underlying error: " + underlyingError).font(.body)
+            if codingPath.count > 0 {
+                Text("Coding path, if any: " + codingPath).font(.body)
+            }
+            if underlyingError.count > 0 {
+                Text("Underlying error: " + underlyingError).font(.body)
+            }
+            Spacer()
         }
         .padding()
     }
