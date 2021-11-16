45af839f9eeaa5445e91cddfa6ed2e5adfbb6d1d
Frederick Kuhl <fkuhl@tyndalesoft.com>
2021-11-16 10:28:43 -0500

    Strengthen household/member checking

--- Shared/MemberHouseholdChecker.swift
+++ Shared/MemberHouseholdChecker.swift
@@ -14,12 +14,14 @@ struct MemberHouseholdChecker {
     func check() -> [String] {
         NSLog("begin member-household check")
         var report = [String]()
+        ///For each member, does the listed household contain the member?
         for member in document.activeMembers {
             let householdOfRecord = document.household(byId: member.household)
             let status = householdOfRecord.contains(member: member.id)
             if status == .notMember {
                 report.append("\(member.fullName()) not in household of record, id '\(member.household)'")
             }
+            ///Is there any other household that purports to contain the member?
             for household in document.households {
                 if household.id == member.household { continue }
                 let status = household.contains(member: member.id)
@@ -28,11 +30,30 @@ struct MemberHouseholdChecker {
                 }
             }
         }
+        ///For each household, does each member it contains point back to it?
+        for household in document.households {
+            checkReverse(memberId: household.head, household: household, report: &report)
+            if let spouseId = household.spouse {
+                checkReverse(memberId: spouseId, household: household, report: &report)
+            }
+            for otherId in household.others {
+                checkReverse(memberId: otherId, household: household, report: &report)
+            }
+        }
         if report.count < 1 {
             report = ["no results"] //indicate something!
         }
         NSLog("end member-household check")
         return report
+    }
+    
+    private func checkReverse(memberId: ID,
+                              household: NormalizedHousehold,
+                              report: inout [String]) {
+        let member = document.member(byId: memberId)
+        if member.household != household.id {
+            report.append("Household listed for member '\(member.fullName())' does not agree with household '\(household.name ?? "[none]")' (ID \(household.id))")
+        }
     }
     
     
